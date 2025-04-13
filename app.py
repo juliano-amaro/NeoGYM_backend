e57015dd6a0c7e1d116c2a8c66920c6a06163705
juliano-amaro <juliano.correa.senai@gmail.com>
2025-04-13 16:01:02 -0300

    primeiro commit

---
+++ app.py
@@ -0,0 +1,118 @@
+from flask import Flask, jsonify, request
+import firebase_admin
+from firebase_admin import credentials, firestore
+from flask_cors import CORS
+import os
+import json
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+CORS(app)
+
+load_dotenv()
+
+FBKEY = json.loads(os.getenv('CONFIG_FIREBASE'))
+
+cred = credentials.Certificate(FBKEY)
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+@app.route('/')
+def index():
+    return 'API TÁ FUNCIONANDO'
+
+@app.route('/academia', methods=['GET'])
+def consulta_lista():
+    alunos = []
+    lista = db.collection('aluno').stream()
+    for item in lista:
+        alunos.append(item.to_dict())
+
+    if alunos:
+        return jsonify(alunos), 200
+    
+    else:
+        return jsonify({{'mensagem': 'Erro! nenhum aluno registrado!'}}), 400
+
+@app.route('/academia/consulta', methods=['POST'])
+def busca_cpf():
+    
+    dados = request.json
+
+    if "cpf" not in dados:
+        return jsonify({'mensagem':'Erro! Campo CPF é obrigatório'})
+
+    cpf = dados['cpf']
+    
+    alunos_ref = db.collection('aluno')
+    busca = alunos_ref.where('cpf', '==', cpf).stream()
+    
+    aluno_encontrado = None
+    for doc in busca:
+        aluno_encontrado = doc.to_dict()
+        break
+
+    if aluno_encontrado:
+        return jsonify({'aluno': aluno_encontrado}), 200
+    else:
+        return jsonify({'mensagem':'Aluno não encontrado'}), 404
+    
+@app.route('/academia', methods=['POST'])
+def cadastro_aluno():
+
+    dados = request.json
+
+    if 'cpf' not in dados or 'nome' not in dados or 'cpf' not in dados:
+        return jsonify({'mensagem': 'Erro! Campos cpf, nome e status são obrigatórios!'}), 400
+    
+    # Contador
+    contador_ref = db.collection('controle_id').document('contador')
+    contador_doc = contador_ref.get().to_dict()
+    ultimo_id = contador_doc.get('id')
+    novo_id = int(ultimo_id) + 1
+    contador_ref.update({'id': novo_id})
+
+    db.collection('aluno').document(str(novo_id)).set({
+        "id": novo_id,
+        "cpf": dados['cpf'],
+        "nome": dados['nome'],
+        "status": dados['status']
+    })
+
+    return jsonify({'mensagem':'Sucesso!! Aluno cadastrado!'}), 201
+   
+@app.route('/academia/update/aluno/<id>', methods=['PUT'])
+def update_aluno(id):
+    dados = request.json
+
+    if 'cpf' not in dados or 'nome' not in dados or 'cpf' not in dados:
+        return jsonify({'mensagem': 'Erro! Campos cpf, nome e status são obrigatórios!'}), 400
+
+    doc_ref = db.collection('aluno').document(id)
+    doc = doc_ref.get()
+
+    if doc.exists:
+        doc_ref.update({
+            "cpf": dados['cpf'],
+            "nome": dados['nome'],
+            "status": dados['status']
+        })
+        return jsonify({'mensagem':'Sucesso! Aluno Editado!'}), 201
+    
+    else:
+        return jsonify({'mensagem':'Erro! Aluno não encontrado!'}), 404
+    
+@app.route('/academia/delete/aluno/<id>', methods=['DELETE'])
+def deletar_aluno(id):
+    doc_ref = db.collection('aluno').document(id)
+    doc = doc_ref.get()
+
+    if not doc.exists:
+        return jsonify({'mensagem': 'Erro! Aluno não encontrado!'})
+    
+    doc_ref.delete()
+    return jsonify({'mensagem': 'Aluno excluída com sucesso!'}), 200
+
+if __name__ == '__main__':
+    app.run()
